7ce4a1d2bad710c8bb79fb42e86a5f9d732b4c53
Astroleander <astroleander@gmail.com>
2021-02-17 00:02:58 +0800

    [sp1] init project

---
+++ project/app/src/main/java/uta/advse6324/ubs/Login/SplashActivity.java
@@ -0,0 +1,15 @@
+package uta.advse6324.ubs.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import uta.advse6324.ubs.R;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
